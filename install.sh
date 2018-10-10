0a5cd3e23f758b4c97c24abe576f4f09323eaf78
Igor Bakulin <IBakulin@luxoft.com>
2018-10-10 15:48:13 +0300

    rename

---
+++ install.sh
@@ -0,0 +1,3 @@
+mkdir -p ~/.vim/autoload ~/.vim/bundle
+curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+git clone https://github.com/scrooloose/nerdtree.git ~/.vim/bundle/nerdtree
